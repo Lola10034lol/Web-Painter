c8fcdcafbc275dcf4c6d9d0c24776bad27c8830f
Lola10034lol <adaoquimeyojeda@gmail.com>
2026-01-13 02:11:17 -0300

    Remove drawing logic from script.js
    Removed drawing functionality and added a comment indicating the file is useless.

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-let drawing = false;
-
-canvas.addEventListener("mousedown", () => drawing = true);
-canvas.addEventListener("mouseup", () => drawing = false);
-canvas.addEventListener("mousemove", draw);
-
-function draw(e) {
-  if (!drawing) return;
-
-  ctx.fillStyle = "black";
-  ctx.beginPath();
-  ctx.arc(e.offsetX, e.offsetY, 4, 0, Math.PI * 2);
-  ctx.fill();
-}
+//Useless script.js file.
